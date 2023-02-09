b5b7210ac68cfeaae909b1d6524f1317b1fe72f8
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-09 13:42:15 -0300

    chore: vite and vitest updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: { enviroment: "happy-dom" },
+  test: {
+    environment: "happy-dom",
+  },
 });
